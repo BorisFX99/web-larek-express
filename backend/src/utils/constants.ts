65ed9cb0ae6455871ad7def0b93deb61e1abb8d6
Boris Vergasov <starsfer839@yandex.ru>
2026-04-21 23:07:18 +0300

    Добавил порт 80 для тестов как резервный

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -5,7 +5,7 @@ import path from 'path';
 dotenv.config();
 
 // env config
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 80;
 export const DB_ADDRESS = process.env.DB_ADDRESS || 'mongodb://127.0.0.1:27017/weblarek';
 export const UPLOAD_PATH = process.env.UPLOAD_PATH || 'images';
 export const UPLOAD_PATH_TEMP = process.env.UPLOAD_PATH_TEMP || 'temp';
